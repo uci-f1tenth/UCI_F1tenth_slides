3b69f0e588286c6dbde48a79bde4836a7d7addf3
Alistair Keiller <alistair@keiller.net>
2025-10-09 15:14:56 -0700

    add more about what pid is

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -19,7 +19,9 @@ class PID:
         self.integral = 0.0
         self.previous_error = None
 
-    def update(self, measurement: float, dt: Optional[float]) -> float:
+    def update(
+        self, measurement: float, dt: Optional[float]
+    ) -> Tuple[float, float, float, float]:
         error: float = self.setpoint - measurement
         if dt and dt > 0.0:
             self.integral += error * dt
@@ -35,7 +37,7 @@ class PID:
         if high is not None and u > high:
             u = high
         self.previous_error = error
-        return u
+        return u, self.kp * error, self.ki * self.integral, self.kd * derivative
 
 
 class Lab1(Slide):
@@ -96,7 +98,7 @@ class Lab1(Slide):
             x, y, _ = mob.get_center()
 
             e = y - line_start[1]
-            omega = pid.update(e, dt)
+            omega, _, _, _ = pid.update(e, dt)
 
             new_theta = heading.get_value() + omega * dt
             mob.rotate(new_theta - heading.get_value())
@@ -204,7 +206,6 @@ class Lab1(Slide):
         self.play(Write(axes))
         self.play(Write(legend_group))
 
-        error_tracker = ValueTracker(0)
         time_tracker = ValueTracker(0)
         error_points = []
         steering_points = []
@@ -226,9 +227,8 @@ class Lab1(Slide):
             x, y, _ = mob.get_center()
 
             e = y - line_start[1]
-            omega = pid.update(e, dt)
+            omega, _, _, _ = pid.update(e, dt)
 
-            error_tracker.set_value(e)
             time_tracker.set_value(time_tracker.get_value() + dt)
             error_points.append([time_tracker.get_value(), e, 0])
             steering_points.append([time_tracker.get_value(), omega, 0])
@@ -303,26 +303,22 @@ class Lab1(Slide):
             .scale(0.8)
             .set_color_by_tex_to_color_map(
                 {
-                    r"e(t)": GREEN,
-                    r"\int_0^t e(\tau) d\tau": RED,
-                    r"\frac{de(t)}{dt}": BLUE,
+                    r"e(t)": YELLOW,
+                    r"\int_0^t e(\tau) d\tau": BLUE,
+                    r"\frac{de(t)}{dt}": PURPLE,
                 }
             )
         )
-        p_legend = Line([0, 0, 0], [0.5, 0, 0], color=GREEN, stroke_width=3)
-        p_label = TexText("Proportional", font_size=20, color=GREEN).next_to(
+        p_legend = Line([0, 0, 0], [0.5, 0, 0], color=YELLOW, stroke_width=3)
+        p_label = TexText("Proportional", font_size=20).next_to(
             p_legend, RIGHT, buff=0.1
         )
 
-        i_legend = Line([0, 0, 0], [0.5, 0, 0], color=RED, stroke_width=3)
-        i_label = TexText("Integral", font_size=20, color=RED).next_to(
-            i_legend, RIGHT, buff=0.1
-        )
+        i_legend = Line([0, 0, 0], [0.5, 0, 0], color=BLUE, stroke_width=3)
+        i_label = TexText("Integral", font_size=20).next_to(i_legend, RIGHT, buff=0.1)
 
-        d_legend = Line([0, 0, 0], [0.5, 0, 0], color=BLUE, stroke_width=3)
-        d_label = TexText("Derivative", font_size=20, color=BLUE).next_to(
-            d_legend, RIGHT, buff=0.1
-        )
+        d_legend = Line([0, 0, 0], [0.5, 0, 0], color=PURPLE, stroke_width=3)
+        d_label = TexText("Derivative", font_size=20).next_to(d_legend, RIGHT, buff=0.1)
 
         pid_legend_group = (
             VGroup(
@@ -342,4 +338,164 @@ class Lab1(Slide):
         self.play(
             FadeOut(pid_legend_group),
             FadeOut(pid_equation_colored),
+        )
+
+        # go back to look at animation with plots for PID error and steering
+        what_is_pid_title = TexText("Another another look at pid")
+        self.play(Write(what_is_pid_title))
+
+        line_start = [-5, -3, 0]
+        line_length = 10
+        pid = PID(kp=2.0, ki=0.1, kd=2.0, setpoint=0.0, out_limits=(-2.0, 2.0))
+        speed = ValueTracker(0)
+        max_speed = 1.5
+        acceleration = 2
+        heading = ValueTracker(PI / 4)
+        line = Line(line_start, line_start + RIGHT * line_length)
+
+        self.play(Transform(what_is_pid_title, line))
+
+        axes = Axes(
+            x_range=(0, 8),
+            y_range=(-2, 2, 0.5),
+            height=6,
+            width=10,
+        ).shift(UP * 0.5)
+
+        axes.add_coordinate_labels(
+            font_size=20,
+            num_decimal_places=1,
+        )
+
+        error_legend = Line([0, 0, 0], [0.5, 0, 0], color=RED, stroke_width=2)
+        error_label = TexText("Error", font_size=20, color=RED).next_to(
+            error_legend, RIGHT, buff=0.1
+        )
+
+        p_legend = Line([0, 0, 0], [0.5, 0, 0], color=YELLOW, stroke_width=3)
+        p_label = TexText("Proportional", font_size=20).next_to(
+            p_legend, RIGHT, buff=0.1
+        )
+
+        i_legend = Line([0, 0, 0], [0.5, 0, 0], color=BLUE, stroke_width=3)
+        i_label = TexText("Integral", font_size=20).next_to(i_legend, RIGHT, buff=0.1)
+
+        d_legend = Line([0, 0, 0], [0.5, 0, 0], color=PURPLE, stroke_width=3)
+        d_label = TexText("Derivative", font_size=20).next_to(d_legend, RIGHT, buff=0.1)
+
+        legend_group = (
+            VGroup(
+                VGroup(error_legend, error_label),
+                VGroup(p_legend, p_label),
+                VGroup(i_legend, i_label),
+                VGroup(d_legend, d_label),
+            )
+            .arrange(DOWN, aligned_edge=LEFT)
+            .to_corner(UR, buff=0.5)
+        )
+
+        self.play(Write(axes))
+        self.play(Write(legend_group))
+
+        time_tracker = ValueTracker(0)
+        error_points = []
+        error_segments = []
+        proportional_points = []
+        proportional_segments = []
+        integral_points = []
+        integral_segments = []
+        derivative_points = []
+        derivative_segments = []
+
+        car = (
+            ImageMobject("labs/lab1/car_topview.png")
+            .scale(0.07)
+            .shift(line_start)
+            .rotate(heading.get_value())
+        )
+
+        self.play(FadeIn(car))
+
+        def follow_path(mob, dt):
+            if not dt or dt <= 0:
+                return
+            x, y, _ = mob.get_center()
+
+            e = y - line_start[1]
+            omega, p, i, d = pid.update(e, dt)
+
+            time_tracker.set_value(time_tracker.get_value() + dt)
+            error_points.append([time_tracker.get_value(), e, 0])
+            proportional_points.append([time_tracker.get_value(), p, 0])
+            integral_points.append([time_tracker.get_value(), i, 0])
+            derivative_points.append([time_tracker.get_value(), d, 0])
+
+            if len(error_points) > 1:
+                error_segment = Line(
+                    axes.coords_to_point(*error_points[-2][:2]),
+                    axes.coords_to_point(*error_points[-1][:2]),
+                    color=RED,
+                    stroke_width=2,
+                )
+                proportional_segment = Line(
+                    axes.coords_to_point(*proportional_points[-2][:2]),
+                    axes.coords_to_point(*proportional_points[-1][:2]),
+                    color=YELLOW,
+                    stroke_width=2,
+                )
+                integral_segment = Line(
+                    axes.coords_to_point(*integral_points[-2][:2]),
+                    axes.coords_to_point(*integral_points[-1][:2]),
+                    color=BLUE,
+                    stroke_width=2,
+                )
+                derivative_segment = Line(
+                    axes.coords_to_point(*derivative_points[-2][:2]),
+                    axes.coords_to_point(*derivative_points[-1][:2]),
+                    color=PURPLE,
+                    stroke_width=2,
+                )
+                error_segments.append(error_segment)
+                proportional_segments.append(proportional_segment)
+                integral_segments.append(integral_segment)
+                derivative_segments.append(derivative_segment)
+                self.add(
+                    error_segment,
+                    proportional_segment,
+                    integral_segment,
+                    derivative_segment,
+                )
+
+            new_theta = heading.get_value() + omega * dt
+            mob.rotate(new_theta - heading.get_value())
+            heading.set_value(new_theta)
+
+            if x < line_start[0] + line_length:
+                speed.set_value(min(speed.get_value() + acceleration * dt, max_speed))
+            else:
+                speed.set_value(max(speed.get_value() - acceleration * dt, 0))
+            if speed.get_value() <= 0:
+                mob.remove_updater(follow_path)
+            dx = speed.get_value() * np.cos(new_theta) * dt
+            dy = speed.get_value() * np.sin(new_theta) * dt
+            mob.move_to([x + dx, y + dy, 0])
+
+        car.add_updater(follow_path)
+        self.wait_until(lambda: follow_path not in car.updaters)
+        self.play(
+            FadeOut(car),
+            FadeOut(what_is_pid_title),
+            FadeOut(axes),
+            *[FadeOut(segment) for segment in error_segments],
+            *[FadeOut(segment) for segment in proportional_segments],
+            *[FadeOut(segment) for segment in integral_segments],
+            *[FadeOut(segment) for segment in derivative_segments],
+            FadeOut(error_legend),
+            FadeOut(error_label),
+            FadeOut(p_legend),
+            FadeOut(p_label),
+            FadeOut(i_legend),
+            FadeOut(i_label),
+            FadeOut(d_legend),
+            FadeOut(d_label),
         )
