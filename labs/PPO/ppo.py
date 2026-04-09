932dc16c2f87c912f262990ff7773f7f49b67a55
Alistair Keiller <alistair@keiller.net>
2026-04-09 11:30:51 -0700

    manim_imports_ext.py

--- labs/PPO/ppo.py
+++ labs/PPO/ppo.py
@@ -1,1 +1,9 @@
-from manimlib import *
+from manim_imports_ext import *
+
+
+class Intro(InteractiveScene):
+    def construct(self):
+        title = Text("PPO").scale(2)
+        self.play(Write(title))
+        self.wait()
+        self.play(FadeOut(title))
